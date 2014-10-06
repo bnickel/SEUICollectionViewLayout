556907aa232e4a8dc89fcb2a7bc88e6aca21f7ae
Brian Nickel <bnickel@stackexchange.com>
2014-10-06 12:51:16 -0700

    Added initial code for SurveyCollectionViewLayout. (Got Swift to consume base collection view.)

---
+++ Code Survey/Code Survey/SurveyCollectionViewLayout.swift
@@ -0,0 +1,17 @@
+//
+//  SurveyCollectionViewLayout.swift
+//  Code Survey
+//
+//  Created by Brian Nickel on 10/6/14.
+//  Copyright (c) 2014 Brian Nickel. All rights reserved.
+//
+
+import UIKit
+
+class SurveyCollectionViewLayout: SEUICollectionViewLayout {
+   
+}
+
+protocol UICollectionViewDelegateSurveyLayout : UICollectionViewDelegateSEUILayout {
+    
+}
