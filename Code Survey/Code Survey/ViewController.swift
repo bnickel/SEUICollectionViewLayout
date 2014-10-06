b5d77fa011e5e818c90edcce32348ac1e00b942a
Brian Nickel <bnickel@stackexchange.com>
2014-10-06 12:36:19 -0700

    Initial view controller setup.

---
+++ Code Survey/Code Survey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Code Survey
+//
+//  Created by Brian Nickel on 10/6/14.
+//  Copyright (c) 2014 Brian Nickel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
